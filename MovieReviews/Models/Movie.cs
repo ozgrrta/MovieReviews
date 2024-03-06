211a0f274da49517f6abfb6ee4e94c49be875457
Özgür Taylan Akdağ <oz.grrta@gmail.com>
2024-03-06 12:12:50 +0300

    -Ilist -> ICollection

--- MovieReviews/Models/Movie.cs
+++ MovieReviews/Models/Movie.cs
@@ -2,7 +2,7 @@
 {
 	public class Movie
 	{
-		public IList<Review> Reviews { get; set; }
+		public ICollection<Review> Reviews { get; set; }
 		public Guid Id { get; set; }
 		public string Name { get; set; }
 
