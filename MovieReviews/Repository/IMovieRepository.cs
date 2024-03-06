7d574c6b1cc77233cb046efbada9910b3dacc576
Özgür Taylan Akdağ <oz.grrta@gmail.com>
2024-03-06 12:13:53 +0300

    -new method for MovieRepositoy

--- MovieReviews/Repository/IMovieRepository.cs
+++ MovieReviews/Repository/IMovieRepository.cs
@@ -4,8 +4,8 @@ namespace MovieReviews.Repository
 {
 	public interface IMovieRepository
 	{
+		Task<List<Movie>> GetMoviesAsync();
 		Task<Movie> GetMovieByIdAsync(Guid id);
 		Task<Movie> AddReviewToMovieAsync(Guid id, Review review);
-
 	}
 }
